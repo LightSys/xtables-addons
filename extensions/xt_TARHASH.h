417116a24fb7862bcb863696272473481085496a
Developer <devel@kernfw.lightsys.org>
2020-03-05 11:15:10 -0700

    Added new members for IPv6 and storing the masks

--- extensions/xt_TARHASH.h
+++ extensions/xt_TARHASH.h
@@ -9,7 +9,10 @@ enum xt_tarhash_target_variant {
 
 struct xt_tarhash_tginfo {
 	uint8_t  variant;
-	uint8_t  src_prefix;
+	uint8_t  src_prefix4;
+	uint8_t  src_prefix8;
+	uint32_t mask4;
+	uint32_t mask6[4];
 	uint32_t ratio;
 	char key[32];
 };
