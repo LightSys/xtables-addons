53bc169fec9750ce2364b1954dbec51b3990d4cf
Developer <devel@kernfw.lightsys.org>
2020-03-05 15:38:37 -0700

    Removed TAR* specific code

--- extensions/xt_TARHASH.h
+++ extensions/xt_TARHASH.h
@@ -1,14 +1,7 @@
 #ifndef _LINUX_NETFILTER_XT_TARHASH_H
 #define _LINUX_NETFILTER_XT_TARHASH_H 1
 
-enum xt_tarhash_target_variant {
-	XTTARHASH_TARPIT,
-	XTTARHASH_HONEYPOT,
-	XTTARHASH_RESET,
-};
-
 struct xt_tarhash_tginfo {
-	uint8_t  variant;
 	uint8_t  src_prefix4;
 	uint32_t mask4;
 #ifdef WITH_IPV6
