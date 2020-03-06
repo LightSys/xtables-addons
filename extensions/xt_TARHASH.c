454e90a7b783a1fc34b85578ea2bbfa973e6a45d
Philip White <philipwhite@cedarville.edu>
2020-03-05 17:10:33 -0700

    Fix compile errors from rebase

--- extensions/xt_TARHASH.c
+++ extensions/xt_TARHASH.c
@@ -66,7 +66,7 @@ struct xt_tarhash_sdesc {
 	char ctx[];
 };
 
-static printkhash(char *hash) {
+static void printkhash(char *hash) {
 	size_t i = 0;
 	const char *hex = "0123456789abcdef";
 	char hex_string_hash[65];
@@ -110,7 +110,6 @@ static bool xttarhash_hashdecided(const struct tcphdr *oth, const struct iphdr *
 	printk("ratio: %u\n", info->ratio);
 	printk("key: %s\n", info->key);
 
-	int hash = 0;
         char string_to_hash[21];
         uint32_t indexed_source_ip = iph->saddr && info->mask4;
 
@@ -122,13 +121,12 @@ static bool xttarhash_hashdecided(const struct tcphdr *oth, const struct iphdr *
 
 static bool xttarhash_hashdecided6(const struct tcphdr *oth, const struct ipv6hdr *iph, const struct xt_tarhash_mtinfo *info) 
 {
-	int hash = 0;
         char string_to_hash[69];
 	
-	__u8 *sa = iph->saddr.s6_addr;
-	__u8 *da = iph->daddr.in6_u.u6_addr8;
+	const __u8 *sa = iph->saddr.s6_addr;
+	const __u8 *da = iph->daddr.in6_u.u6_addr8;
 
-	uint8_t *ma = info->mask6;
+	const uint8_t *ma = info->mask6;
 
 	snprintf(string_to_hash, 69, "%02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %04x",
 		 sa[0] && ma[0], sa[1] && ma[1], sa[2] && ma[2], sa[3] && ma[3],
@@ -139,156 +137,38 @@ static bool xttarhash_hashdecided6(const struct tcphdr *oth, const struct ipv6hd
 		 da[4], da[5], da[6], da[7], da[8], da[9], da[10], da[11], da[12],
 		 da[13], da[14], da[15], oth->dest);
 
-	return xttarhash_decision(string_to_hash, strlen(string_to_hash));
+	return xttarhash_decision(info, string_to_hash, strlen(string_to_hash));
 }
 
-static void tarhash_tcp4(struct net *net, struct sk_buff *oldskb,
+static bool tarhash_tcp4(struct net *net, const struct sk_buff *oldskb,
     unsigned int hook, const struct iphdr *iph, const struct xt_tarhash_mtinfo *info)
 {
-	struct tcphdr _otcph, *tcph;
+	struct tcphdr _otcph;
 	const struct tcphdr *oth;
-	unsigned int addr_type = RTN_UNSPEC;
-	struct sk_buff *nskb;
-	const struct iphdr *oldhdr;
-	struct iphdr *niph;
-	uint16_t tmp, payload;
 	
-	unsigned int mode;
-
-	mode = info->variant;
-
 	/* A truncated TCP header is not going to be useful */
 	if (oldskb->len < ip_hdrlen(oldskb) + sizeof(struct tcphdr))
-		return;
+		return false;
 
 	oth = skb_header_pointer(oldskb, ip_hdrlen(oldskb),
 	                         sizeof(_otcph), &_otcph);
 	if (oth == NULL)
-		return;
+		return false;
 
 	/* Check using hash function whether tarpit response should be sent */
-	if (!xttarhash_hashdecided(oth, iph, info)) 
-		goto free_nskb;
-
-	/* Check checksum. */
-	if (nf_ip_checksum(oldskb, hook, ip_hdrlen(oldskb), IPPROTO_TCP))
-		return;
-
-	/*
-	 * Copy skb (even if skb is about to be dropped, we cannot just
-	 * clone it because there may be other things, such as tcpdump,
-	 * interested in it)
-	 */
-	nskb = skb_copy_expand(oldskb, LL_MAX_HEADER,
-	                       skb_tailroom(oldskb), GFP_ATOMIC);
-	if (nskb == NULL)
-		return;
-
-	/* This packet will not be the same as the other: clear nf fields */
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 4, 0)
-	nf_reset_ct(nskb);
-#else
-	nf_reset(nskb);
-#endif
-	skb_nfmark(nskb) = 0;
-	skb_init_secmark(nskb);
-	skb_shinfo(nskb)->gso_size = 0;
-	skb_shinfo(nskb)->gso_segs = 0;
-	skb_shinfo(nskb)->gso_type = 0;
-	oldhdr = ip_hdr(oldskb);
-	tcph = (struct tcphdr *)(skb_network_header(nskb) + ip_hdrlen(nskb));
-
-	/* Swap source and dest */
-	niph         = ip_hdr(nskb);
-	niph->daddr  = xchg(&niph->saddr, niph->daddr);
-	tmp          = tcph->source;
-	tcph->source = tcph->dest;
-	tcph->dest   = tmp;
-
-	/* Calculate payload size?? */
-	payload = nskb->len - ip_hdrlen(nskb) - sizeof(struct tcphdr);
-
-	/* Truncate to length (no data) */
-	tcph->doff    = sizeof(struct tcphdr) / 4;
-	skb_trim(nskb, ip_hdrlen(nskb) + sizeof(struct tcphdr));
-	niph->tot_len = htons(nskb->len);
-	tcph->urg_ptr = 0;
-	/* Reset flags */
-	((u_int8_t *)tcph)[13] = 0;
-
-	/* Adjust TCP checksum */
-	tcph->check = 0;
-	tcph->check = tcp_v4_check(sizeof(struct tcphdr), niph->saddr,
-	              niph->daddr, csum_partial((char *)tcph,
-	              sizeof(struct tcphdr), 0));
-
-	/* Set DF, id = 0 */
-	niph->frag_off = htons(IP_DF);
-	if (mode == XTTARHASH_TARPIT || mode == XTTARHASH_RESET)
-		niph->id = 0;
-	else if (mode == XTTARHASH_HONEYPOT)
-		niph->id = ~oldhdr->id + 1;
-
-#ifdef CONFIG_BRIDGE_NETFILTER
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 0, 0)
-	if (hook != NF_INET_FORWARD || ((struct nf_bridge_info *)skb_ext_find(nskb, SKB_EXT_BRIDGE_NF) != NULL &&
-	    ((struct nf_bridge_info *)skb_ext_find(nskb, SKB_EXT_BRIDGE_NF))->physoutdev))
-#else
-	if (hook != NF_INET_FORWARD || (nskb->nf_bridge != NULL &&
-	    nskb->nf_bridge->physoutdev != NULL))
-#endif
-#else
-	if (hook != NF_INET_FORWARD)
-#endif
-		addr_type = RTN_LOCAL;
-
-	if (ip_route_me_harder(net, nskb, addr_type))
-		goto free_nskb;
-	else
-		niph = ip_hdr(nskb);
-
-	nskb->ip_summed = CHECKSUM_NONE;
-
-	/* Adjust IP TTL */
-	if (mode == XTTARHASH_HONEYPOT)
-		niph->ttl = 128;
-	else
-		niph->ttl = ip4_dst_hoplimit(skb_dst(nskb));
-
-	/* Adjust IP checksum */
-	niph->check = 0;
-	niph->check = ip_fast_csum(skb_network_header(nskb), niph->ihl);
-
-	/* "Never happens" */
-	if (nskb->len > dst_mtu(skb_dst(nskb)))
-		goto free_nskb;
-
-	nf_ct_attach(nskb, oldskb);
-	NF_HOOK(NFPROTO_IPV4, NF_INET_LOCAL_OUT, net, nskb->sk, nskb, NULL,
-		skb_dst(nskb)->dev, dst_output);
-	return;
-
- free_nskb:
-	kfree_skb(nskb);
+	return xttarhash_hashdecided(oth, iph, info);
 }
 
 #ifdef WITH_IPV6
-static void tarhash_tcp6(struct net *net, struct sk_buff *oldskb,
+static bool tarhash_tcp6(struct net *net, const struct sk_buff *oldskb,
     unsigned int hook, const struct ipv6hdr *iph, const struct xt_tarhash_mtinfo *info)
 {
-	struct sk_buff *nskb;
-	struct tcphdr *tcph, oth;
+	struct tcphdr oth;
 	unsigned int otcplen;
 	int tcphoff;
 	const struct ipv6hdr *oip6h = ipv6_hdr(oldskb);
-	struct ipv6hdr *ip6h;
-	const uint8_t tclass = 0;
 	uint8_t proto;
-	uint16_t payload;
 	__be16 frag_off;
-	unsigned int mode;
-
-	mode = info->variant;
 
 	proto   = oip6h->nexthdr;
 	tcphoff = ipv6_skip_exthdr(oldskb,
@@ -296,7 +176,7 @@ static void tarhash_tcp6(struct net *net, struct sk_buff *oldskb,
 
 	if (tcphoff < 0 || tcphoff > oldskb->len) {
 		pr_debug("Cannot get TCP header.\n");
-		return;
+		return false;
 	}
 
 	otcplen = oldskb->len - tcphoff;
@@ -306,94 +186,23 @@ static void tarhash_tcp6(struct net *net, struct sk_buff *oldskb,
 		pr_debug("proto(%d) != IPPROTO_TCP, "
 		         "or too short. otcplen = %d\n",
 		         proto, otcplen);
-		return;
+		return false;
 	}
 
 	if (skb_copy_bits(oldskb, tcphoff, &oth, sizeof(struct tcphdr))) {
 		WARN_ON(1);
-		return;
+		return false;
 	}
 
 	/* Check checksum. */
 	if (csum_ipv6_magic(&oip6h->saddr, &oip6h->daddr, otcplen, IPPROTO_TCP,
 	    skb_checksum(oldskb, tcphoff, otcplen, 0))) {
 		pr_debug("TCP checksum is invalid\n");
-		return;
+		return false;
 	}
 
 	/* Check using hash function whether tarpit response should be sent */
-	if (!xttarhash_hashdecided6(&oth, iph, info)) 
-		goto free_nskb;
-
-
-	nskb = skb_copy_expand(oldskb, LL_MAX_HEADER,
-	       skb_tailroom(oldskb), GFP_ATOMIC);
-	if (nskb == NULL) {
-		if (net_ratelimit())
-			pr_debug("cannot alloc skb\n");
-		return;
-	}
-
-	/* This packet will not be the same as the other: clear nf fields */
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 4, 0)
-	nf_reset_ct(nskb);
-#else
-	nf_reset(nskb);
-#endif
-	skb_nfmark(nskb) = 0;
-	skb_init_secmark(nskb);
-	skb_shinfo(nskb)->gso_size = 0;
-	skb_shinfo(nskb)->gso_segs = 0;
-	skb_shinfo(nskb)->gso_type = 0;
-	skb_put(nskb, sizeof(struct ipv6hdr));
-	ip6h = ipv6_hdr(nskb);
-	*(__be32 *)ip6h =  htonl(0x60000000 | (tclass << 20));
-	ip6h->nexthdr = IPPROTO_TCP;
-	ip6h->saddr = oip6h->daddr;
-	ip6h->daddr = oip6h->saddr;
-
-	/* Adjust IP TTL */
-	if (mode == XTTARHASH_HONEYPOT) {
-		ip6h->hop_limit = 128;
-	} else {
-		ip6h->hop_limit = ip6_dst_hoplimit(skb_dst(nskb));
-	}
-
-	tcph = (struct tcphdr *)(skb_network_header(nskb) +
-	       sizeof(struct ipv6hdr));
-
-	/* Truncate to length (no data) */
-	skb_trim(nskb, sizeof(struct ipv6hdr) + sizeof(struct tcphdr));
-	tcph->doff    = sizeof(struct tcphdr)/4;
-	tcph->source  = oth.dest;
-	tcph->dest    = oth.source;
-	tcph->urg_ptr = 0;
-	/* Reset flags */
-	((uint8_t *)tcph)[13] = 0;
-
-	payload = nskb->len - sizeof(struct ipv6hdr) - sizeof(struct tcphdr);
-
-	ip6h->payload_len = htons(sizeof(struct tcphdr));
-	tcph->check = 0;
-
-	/* Adjust TCP checksum */
-	tcph->check = csum_ipv6_magic(&ipv6_hdr(nskb)->saddr,
-	              &ipv6_hdr(nskb)->daddr, sizeof(struct tcphdr),
-	              IPPROTO_TCP,
-	              csum_partial(tcph, sizeof(struct tcphdr), 0));
-
-	if (ip6_route_me_harder(net, nskb))
-		goto free_nskb;
-
-	nskb->ip_summed = CHECKSUM_NONE;
-
-	nf_ct_attach(nskb, oldskb);
-	NF_HOOK(NFPROTO_IPV6, NF_INET_LOCAL_OUT, net, nskb->sk, nskb, NULL,
-	        skb_dst(nskb)->dev, dst_output);
-	return;
-
- free_nskb:
-	kfree_skb(nskb);
+	return xttarhash_hashdecided6(&oth, iph, info);
 }
 #endif
 
@@ -405,29 +214,28 @@ static bool tarhash_mt4(const struct sk_buff *skb, struct xt_action_param *par)
 
 	/* Do we have an input route cache entry? (Not in PREROUTING.) */
 	if (rt == NULL)
-		return NF_DROP;
+		return false;
 
 	/* No replies to physical multicast/broadcast */
 	/* skb != PACKET_OTHERHOST handled by ip_rcv() */
 	if (skb->pkt_type != PACKET_HOST)
-		return NF_DROP;
+		return false;
 
 	/* Now check at the protocol level */
 	if (rt->rt_flags & (RTCF_BROADCAST | RTCF_MULTICAST))
-		return NF_DROP;
+		return false;
 
 	/*
 	 * Our naive response construction does not deal with IP
 	 * options, and probably should not try.
 	 */
 	if (ip_hdrlen(skb) != sizeof(struct iphdr))
-		return NF_DROP;
+		return false;
 
 	/* We are not interested in fragments */
 	if (iph->frag_off & htons(IP_OFFSET))
-		return NF_DROP;
-	tarhash_tcp4(par_net(par), skb, par->state->hook, iph, info);
-	return NF_DROP;
+		return false;
+	return tarhash_tcp4(par_net(par), skb, par->state->hook, iph, info);
 }
 
 #ifdef WITH_IPV6
@@ -442,14 +250,14 @@ static bool tarhash_mt6(const struct sk_buff *skb, struct xt_action_param *par)
 	/* Do we have an input route cache entry? (Not in PREROUTING.) */
 	if (rt == NULL) {
 		pr_debug("Dropping no input route cache entry\n");
-		return NF_DROP;
+		return false;
 	}
 
 	/* No replies to physical multicast/broadcast */
 	/* skb != PACKET_OTHERHOST handled by ip_rcv() */
 	if (skb->pkt_type != PACKET_HOST) {
 		pr_debug("type != PACKET_HOST");
-		return NF_DROP;
+		return false;
 	}
 
 	/*
@@ -459,15 +267,14 @@ static bool tarhash_mt6(const struct sk_buff *skb, struct xt_action_param *par)
 	proto = iph->nexthdr;
 	if (ipv6_skip_exthdr(skb, skb_network_header_len(skb), &proto,
 	    &frag_off) != sizeof(struct ipv6hdr))
-		return NF_DROP;
+		return false;
 
 	if ((!(ipv6_addr_type(&iph->saddr) & IPV6_ADDR_UNICAST)) ||
 	    (!(ipv6_addr_type(&iph->daddr) & IPV6_ADDR_UNICAST))) {
 		pr_debug("addr is not unicast.\n");
-		return NF_DROP;
+		return false;
 	}
-	tarhash_tcp6(par_net(par), skb, par->state->hook, iph, info);
-	return NF_DROP;
+	return tarhash_tcp6(par_net(par), skb, par->state->hook, iph, info);
 }
 #endif
 
